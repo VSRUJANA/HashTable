d4fe5a13975e92dca76add02b5926e2939350014
VSRUJANA <srujana.valavala@gmail.com>
2020-10-25 13:02:15 +0530

    [srujana] Add. HashTable implemented using LinkedList

--- DS_HashTable/Program.cs
+++ DS_HashTable/Program.cs
@@ -7,6 +7,13 @@ namespace DS_HashTable
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to HashTable Program!");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
         }
     }
 }
